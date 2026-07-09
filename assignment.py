389a901386ccd7383f4d98f4c54ffb9e228edfb8
Pankaj <chypankajchy26@gmail.com>
2026-07-09 20:18:13 +0545

    pushing the code of day1

---
+++ assignment.py
@@ -0,0 +1,7 @@
+print("This is the Program to calculate the area of Circle:")
+a = input("Enter the radius of Circle:")
+a = int(a)
+pi = 3.14
+
+area = pi * a**2
+print("The area of Circle is :", area)
---
+++ assignment2.py
@@ -0,0 +1,7 @@
+print("This is the program that convert Celcius deg to Fahrenheit deg and Kelvin deg:")
+C = float(input("Enter the temperature in Celcius: "))
+
+f = (C* 9/5) + 32
+k = C + 273.15
+print("The temperature in Fahrenheit is: ", f)
+print("The temperature in Kelvin is: ", k)
